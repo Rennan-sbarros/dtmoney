8f2fecf23cc0253c54349791910320eac4ce2185
rennan-sbarros <rennan_sbarros@hotmail.com>
2021-10-06 23:18:17 -0300

    modal stylization

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -78,4 +78,18 @@ export const GlobalStyle = createGlobalStyle`
         position: relative;
         border-radius: 0.24rem;
     }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        &:hover {
+            filter: brightness(0.5)
+        }
+
+        transition: filter 0.1s;
+    }
 `
