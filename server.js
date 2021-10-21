34eaf3716214debee01d72c52c30898cd3da27f6
rennan-sbarros <rennan_sbarros@hotmail.com>
2021-10-21 19:52:13 -0300

    Update Serverjs 2

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ const app = express()
 
 app.use('/', 
     express.static(
-        __dirname,
-        './build'
+        resolve(
+            __dirname,
+            './build'
+        )
     )
 )
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-// app.listen(process.env.PORT || 3000, (err) =>{
-//     if (err) {
-//         return console.log(err)
-//     }
-//     console.log('Tudo funcionando')
+app.listen(process.env.PORT || 3000, (err) =>{
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Tudo funcionando')
     
-// })
+})
