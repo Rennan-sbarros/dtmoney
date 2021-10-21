faafec56b22576f4afca2a7db408ec863c1f1e06
rennan-sbarros <rennan_sbarros@hotmail.com>
2021-10-21 19:36:45 -0300

    Update serverjs

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ app.use('/',
     )
 )
 
-app.listen(process.env.PORT || 3000, (err) =>{
-    if (err) {
-        return console.log(err)
-    }
-    console.log('Tudo funcionando')
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// app.listen(process.env.PORT || 3000, (err) =>{
+//     if (err) {
+//         return console.log(err)
+//     }
+//     console.log('Tudo funcionando')
     
-})
+// })
